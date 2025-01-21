e9eddcdcbba03dfcfb6fc1147b6db62567acfabe
dohyeons <ddhhss0603@gmail.com>
2025-01-21 18:14:50 +0900

    feat: SpinSlow 컴포넌트 구현

--- src/pages/Hero/Hero.tsx
+++ src/pages/Hero/Hero.tsx
@@ -1,3 +1,9 @@
+import SpinSlow from "@/pages/Hero/SpinSlow";
+
 export default function Hero() {
-	return <div>hero</div>;
+	return (
+		<div className="relative mx-auto max-w-[1020px] w-full">
+			<SpinSlow />
+		</div>
+	);
 }
